97726c9f193e73793cd99437845d9f5a363a7fde
Jarryd Son <jdsonza@gmail.com>
2015-01-16 14:21:46 +0200

    added screen functionality

--- DualLogger.ino
+++ DualLogger.ino
@@ -11,19 +11,31 @@
 
 #include <SPI.h>
 #include <SD.h>
+#include <TFT.h>
 
 #define SD_CS 4
+#define LD_CS 10
+#define CD 9
+#define RESET 8
+
+TFT screen = TFT(LD_CS, CD,RESET);
+
 //===================================GLOBAL VARIABLES===============================================================
 int lineCountV = 0;
 int fileCountV = 0;
 int lineCountT = 0;
 int fileCountT = 0;
+int xPos = 0;
 String fileName = "";
 String dataString = "";
 char charFileNameV[12] = "vltlog.log";
 char charFileNameT[12] = "tmplog.log";
 char choice;
+char menu [100];
+String strMenu = "Enter:\n\t'B' to begin\n\t'E' to exit\n\t'T' Temperature\n\t'V' Voltage";
+
 File dataFile;
+
 
 //========================================SETUP======================================================================
 
@@ -31,16 +43,26 @@ void setup()
 {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
-  Serial.print("Initializing SD card...");
+  
   // make sure that the default chip select pin is set to
   // output, even if you don't use it:
   pinMode(10, OUTPUT);
-
-  Serial.println ("Enter 'A' to begin, 'B' to exit");
-  while (choice != 'A') {
+  
+  screen.begin();
+  screen.background (0,0,0);
+  screen.stroke (0,255,0);
+  screen.setRotation(2);
+  strMenu.toCharArray(menu,100);
+  screen.text(menu,0,0);
+  Serial.println (strMenu);
+  while (choice != 'B') {
     choice = Serial.read();
   }
+  
   // see if the card is present and can be initialized:
+  screen.stroke (0,0,0);
+  screen.text(menu,0,0);
+  
   if (!SD.begin(SD_CS)) {
     Serial.println("Card failed, or not present");
     // don't do anything more:
@@ -53,7 +75,7 @@ void setup()
 //===================================================MAIN LOOP====================================================
 void loop()
 {
-  if (choice != 'B')
+  if (choice != 'E')
   {
     getSensorData(0);
     writeToFile("Voltage");
@@ -62,7 +84,13 @@ void loop()
     getSensorData(1);
     writeToFile("Temperature");
     updateCount("Temperature");
-
+    
+    if (choice == 'V'){
+      displayGraph(0);
+    }
+     else {displayGraph(1);}
+    
+    
     delay(1000);
   }
   else {
@@ -72,6 +100,36 @@ void loop()
 }
 
 //=========================================================FUNCTIONS===============================================
+
+void displayGraph (int pin)
+{
+    int sensor = 0;
+    sensor = analogRead(pin);
+    int drawHeight = map(sensor, 0, 1023, 0, screen.height());
+    switch(pin){
+      case 0:
+        screen.stroke (255,0,0);
+        break;
+      case 1:
+        screen.stroke (0,255,120);
+        break;
+      default:
+        screen.stroke (255,255,255);
+        break;
+    }
+    if (xPos >= screen.width()) {
+    xPos = 0;
+    screen.background(0,0,0);
+  }
+  else {
+    // increment the horizontal position:
+    xPos++;
+  }
+  
+  screen.line(xPos, screen.height() - drawHeight,xPos,screen.height());
+
+  delay(16);
+}
 
 void getSensorData(int pin)
 {
@@ -148,6 +206,8 @@ void serialEvent()
     // get the new byte:
     char inChar = (char)Serial.read();
     choice = inChar;
+    screen.background(0,0,0);
+    xPos = 0;
   }
 }
 
